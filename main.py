b793078222296b937c5d56b54ca6036270c263c3
Jaykold <christianichebi@gmail.com>
2025-03-25 20:16:04 +0100

    chore: add main.py file

---
+++ main.py
@@ -0,0 +1,21 @@
+from scripts import fetch_all_stocks, auth_datalake, upload_file_to_datalake
+import asyncio
+from utils import ACC_NAME, ACC_KEY, CONTAINER_NAME 
+from time import perf_counter
+import pandas as pd
+
+async def main():
+    start_time = perf_counter()
+    print(f"Starting fetch at {start_time}")
+    
+    ticker_df = pd.read_csv("data/companies.csv")
+    
+    results = await fetch_all_stocks(ticker_df)
+    service_client = auth_datalake(acc_name=ACC_NAME, acc_key=ACC_KEY)
+    upload_file_to_datalake(results, service_client, CONTAINER_NAME)
+
+    end_time = perf_counter()
+    print(f"Completed at {end_time}, Took {end_time - start_time}")
+
+if __name__=="__main__":
+    asyncio.run(main())
