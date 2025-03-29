dd84bae053664c816295979e6afdfe905fa82146
Jaykold <christianichebi@gmail.com>
2025-03-29 23:30:43 +0100

    "Modify this file"

--- elt/dags/scripts/spark_job.py
+++ elt/dags/scripts/spark_job.py
@@ -2,20 +2,30 @@ import os
 import sys
 from pyspark.sql import SparkSession
 from pyspark.conf import SparkConf
+from pyspark.sql.functions import col, round
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType, TimestampType
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils import ACC_NAME, SP_APP_ID, SP_SECRET_ID, SP_TENANT_ID, CONTAINER_NAME
+from utils import ACC_NAME, SP_APP_ID, SP_SECRET_ID, SP_TENANT_ID, CONTAINER_NAME, ACC_KEY
+
+schema = StructType([
+    StructField("Datetime", TimestampType(), False),
+    StructField("Open", DoubleType(), True),
+    StructField("High", DoubleType(), True),
+    StructField("Low", DoubleType(), True),
+    StructField("Close", DoubleType(), True),
+    StructField("Volume", LongType(), True),
+    StructField("Dividends", DoubleType(), True),
+    StructField("Stock Splits", DoubleType(), True),
+    StructField("symbol", StringType(), True), 
+    StructField("name", StringType(), True)
+])
 
 conf = SparkConf() \
-    .setAppName('adls-job') \
+    .setAppName('StockDataTransformation') \
     .setMaster("local[*]") \
-    .set("spark.jars", "/opt/spark/jars/hadoop-azure-3.3.4.jar,/opt/spark/jars/mssql-jdbc-12.10.0.jrell.jar") \
-    .set("fs.azure.account.auth.type.{}.dfs.core.windows.net".format(ACC_NAME), "OAuth") \
-    .set("fs.azure.account.oauth.provider.type.{}.dfs.core.windows.net".format(ACC_NAME), "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider") \
-    .set("fs.azure.account.oauth2.client.id.{}.dfs.core.windows.net".format(ACC_NAME), SP_APP_ID) \
-    .set("fs.azure.account.oauth2.client.secret.{}.dfs.core.windows.net".format(ACC_NAME), SP_SECRET_ID) \
-    .set("fs.azure.account.oauth2.client.endpoint.{}.dfs.core.windows.net".format(ACC_NAME), "https://login.microsoftonline.com/{}/oauth2/token".format(SP_TENANT_ID)) \
+    .set("spark.hadoop.fs.azure.account.key.{}.dfs.core.windows.net".format(ACC_NAME), ACC_KEY) \
     .set("spark.hadoop.fs.azurebfs.logging.enabled", "true") \
     .set("spark.hadoop.fs.azurebfs.logging.level", "ERROR")
 
@@ -24,9 +34,21 @@ spark = SparkSession.builder.config(conf=conf).getOrCreate()
 file_path = f"abfss://{CONTAINER_NAME}@{ACC_NAME}.dfs.core.windows.net/raw/company_data/*.csv"
 
 try:
-    df = spark.read.csv(file_path)
+    df = spark.read.csv(file_path, header=True, schema=schema)
     df.printSchema()
-    df.show(15)
+    
+    df.select(
+        col('Datetime'),
+        round(col('Open'), 2).alias('Open'),
+        round(col('High'), 2).alias('High'),
+        round(col('Low'), 2).alias('Low'),
+        round(col('Close'), 2).alias('Close'),
+        col('Volume'),
+        round(col('Dividends'), 2).alias('Dividends'),
+        round(col('Stock Splits'), 2).alias('Stock Splits'),
+        col('symbol'),
+        col('name')
+    ).show(15)
 
 except Exception as e:
     print(f"Error occured: {e}")
